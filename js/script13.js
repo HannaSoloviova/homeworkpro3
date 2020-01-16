217ab8a4b80b24252fadd8676434eeb745c72fa1
HannaSoloviova <49920401+HannaSoloviova@users.noreply.github.com>
2020-01-16 08:58:16 +0200

    Update script13.js

--- js/script13.js
+++ js/script13.js
@@ -3,7 +3,7 @@ let N = parseInt(prompt('Введите целое число'));
 for (let a = 1; a <= 100; a++) {
 
 	if (a * a <= N) {
-		console.log(a);
+		console.log('Целое числою, квадрат которого не превышает введенное число' + a);
 	}
 
 }
